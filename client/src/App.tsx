e2e3942724ff87ce8ef3ad45690626c7c4eb8df1
westbrookcook <pvjkdkw0818@hotmail.com>
2025-02-12 19:47:23 +0800

    initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ResumeBot</h1>
+        <p>AI-Powered Resume Generator</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
